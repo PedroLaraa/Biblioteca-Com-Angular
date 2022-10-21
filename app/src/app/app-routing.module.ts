0c52687d802ffb7dc2777c9c437e80e6750a9a8a
Pedro Lara <pedroalveslara@gmail.com>
2022-10-21 11:28:08 -0300

    ble

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -5,6 +5,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    component: ListarPensamentoComponent,
+    pathMatch:'full'
+  },
+  {
     path: 'criar-pensamento',
     component: CriarPensamentoComponent
   },
