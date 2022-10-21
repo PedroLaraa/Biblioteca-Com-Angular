2ce90ce43cfb0abf0d60bf292660b3ec7ed1ad80
Pedro Lara <pedroalveslara@gmail.com>
2022-10-21 11:50:26 -0300

    framework lixo

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,14 +1,9 @@
+import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
+import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 const routes: Routes = [
-  {
-    path: '',
-    component: ListarPensamentoComponent,
-    pathMatch:'full'
-  },
   {
     path: 'criar-pensamento',
     component: CriarPensamentoComponent
@@ -18,12 +13,6 @@ const routes: Routes = [
     component: ListarPensamentoComponent
   }
 ];
-=======
-const routes: Routes = [];
->>>>>>> parent of b5d749a (listagem e criação de pensamentos check)
-=======
-const routes: Routes = [];
->>>>>>> parent of b5d749a (listagem e criação de pensamentos check)
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -6,36 +6,21 @@ import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
 import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { FormsModule } from '@angular/forms';
 import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
-import { PensamentoComponent } from './componentes/pensamentos/pensamento/pensamento.component';
-=======
->>>>>>> parent of b5d749a (listagem e criação de pensamentos check)
-=======
->>>>>>> parent of b5d749a (listagem e criação de pensamentos check)
 
 @NgModule({
   declarations: [
     AppComponent,
     CabecalhoComponent,
     RodapeComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
     CriarPensamentoComponent,
-    ListarPensamentoComponent,
-    PensamentoComponent
-=======
-    CriarPensamentoComponent
->>>>>>> parent of b5d749a (listagem e criação de pensamentos check)
-=======
-    CriarPensamentoComponent
->>>>>>> parent of b5d749a (listagem e criação de pensamentos check)
+    ListarPensamentoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
